082066a46da9cf1f376e844bf7b03138b41fc9fd
Sergey Sergeychik <sergey.sergeychik@gmail.com>
2020-11-17 16:06:46 +0300

    Изменения к задаче №1

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,40 @@
+package ru.netology.service;
+
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+
+public class CashbackHackServiceTest {
+    CashbackHackService cashbackHackService = new CashbackHackService();
+
+    @Test
+    public void boundaryAmount() {
+        int actual = cashbackHackService.remain(1000);
+        int expected = 0;
+        assertEquals(actual, expected);
+
+    }
+
+    @Test
+    public void lessBoundaryAmount() {
+        int actual = cashbackHackService.remain(900);
+        int expected = 100;
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void moreBoundaryAmount() {
+        int actual = cashbackHackService.remain(1200);
+        int expected = 800;
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void zeroBoundaryAmount() {
+        int actual = cashbackHackService.remain(0);
+        int expected = 1000;
+        assertEquals(actual, expected);
+    }
+}
